3c595f1fe0324c4e5361edbbf23c66e34c42d43b
jared0074 <64407750+jared0074@users.noreply.github.com>
2021-08-25 13:15:24 +0800

    Add files via upload

---
+++ RTS_Version_1/src/rts/version/pkg1/Sensor.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rts.version.pkg1;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author jared
+ */
+public class Sensor implements Runnable {
+    int sensorID;
+    String sensorName;
+    SmartWatch display;
+    boolean danger = false;
+
+    
+    public Sensor (SmartWatch display,int sensorNum){
+        this.sensorID = sensorNum;
+        sensorName = "Sensor " + sensorNum;
+        this.display = display;
+    }
+    
+    @Override
+    public void run() {
+        try {
+            display.checkVitals(this);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Sensor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Sensor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+    
+}
---
+++ RTS_Version_2/src/rts/version/pkg2/SensorGenerator.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rts.version.pkg2;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author jared
+ */
+public class SensorGenerator implements Runnable{
+    SmartWatch display;
+    int numSensor = 1;
+    boolean dangerTime;
+
+    public SensorGenerator(SmartWatch display) {
+        this.display = display;
+    }
+
+    @Override
+    public void run() {
+        while (numSensor != 3) {
+            Sensor sensor = new Sensor(display, numSensor);
+            Thread thSensor = new Thread(sensor);
+            thSensor.start();
+            numSensor++;
+        }
+    }
+}
