cf68f672d53794c85e22990a30794d4a3d32d7b1
jared0074 <64407750+jared0074@users.noreply.github.com>
2021-08-25 13:21:56 +0800

    Add files via upload

---
+++ RTS_Version_16/src/rts_2ndedition_6/SmartWatch.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rts_2ndedition_6;
+
+import java.util.Timer;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ *
+ * @author jared
+ */
+public class SmartWatch {
+    Notification notification = new Notification(this);
+    Clock clock = new Clock(this);
+    int min = 30; int max = 100;
+    LinkedBlockingQueue<Integer> heartRate = new LinkedBlockingQueue();
+    LinkedBlockingQueue<Integer> bloodOxygen = new LinkedBlockingQueue();
+    ExecutorService execute = Executors.newCachedThreadPool();
+    boolean BloodCheckFin = false;
+    boolean HeartCheckFin = false;
+    Object sensorWait = new Object();
+    Object notiWait = new Object();
+    boolean HeartRateChecker,BloodOxygenChecker;
+    boolean danger = false;
+    Timer t = new Timer();
+    Timer t1 = new Timer();
+    
+    public SmartWatch(){
+        SensorGenerator senseGen = new SensorGenerator(this);
+        t.schedule(clock,0);
+        execute.execute(senseGen);
+        t1.schedule(notification,0);
+    }
+}
